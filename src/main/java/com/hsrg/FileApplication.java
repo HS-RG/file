964110d9fbe7efa1d19bd7bcf7572b4e6d9fcb1b
wxl <1715142548@qq.com>
2023-06-04 17:04:38 +0800

    启用feign

--- src/main/java/com/hsrg/FileApplication.java
+++ src/main/java/com/hsrg/FileApplication.java
@@ -1,9 +1,13 @@
 package com.hsrg;
 
+import com.hsrg.clients.UserClient;
+import com.hsrg.config.DefaultFeignConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(clients = UserClient.class, defaultConfiguration = DefaultFeignConfiguration.class)
 public class FileApplication {
     public static void main(String[] args) {
         SpringApplication.run(FileApplication.class,args);
